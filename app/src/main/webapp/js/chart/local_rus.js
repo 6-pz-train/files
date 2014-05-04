a49da76ac9eb953c74095bb3e5adc0f5f4f8e5c7
Anatoliy Shkabara <anatoliy.shkabara@gmail.com>
2014-05-04 20:08:38 +0300

    set rus local for charts

---
+++ app/src/main/webapp/js/chart/local_rus.js
@@ -0,0 +1,25 @@
+Highcharts.setOptions({
+    lang: {
+        months: ['Январь', 'Февраль', 'Март', 'Апрель',
+            'Май', 'Июнь', 'Июль', 'Август',
+            'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        weekdays: ['Воскресенье', 'Понедельник', 'Вторник',
+            'Среда', 'Четверг', 'Пятница', 'Суббота'],
+        shortMonths: ['Январь', 'Февраль', 'Март', 'Апрель',
+            'Май', 'Июнь', 'Июль', 'Август',
+            'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        exportButtonTitle: "Экспорт",
+        printButtonTitle: "Импорт",
+        rangeSelectorFrom: "От",
+        rangeSelectorTo: "До",
+        rangeSelectorZoom: "Увеличить",
+        downloadPNG: 'Загрузить как PNG',
+        downloadJPEG: 'Загрузить как JPEG',
+        downloadPDF: 'Загрузить как PDF',
+        downloadSVG: 'Загрузить как SVG',
+        printChart: 'Печать',
+        contextButtonTitle: 'Контекстное меню',
+        resetZoom: 'Сбросить',
+        resetZoomTitle: 'Сбросить увеличение к 1:1'
+    }
+});
