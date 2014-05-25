3114d42b3549530cb6e2d03aa86ec94da5d5a21f
Anatoliy Shkabara <anatoliy.shkabara@gmail.com>
2014-05-25 20:50:39 +0300

    added form users fix interface for user roles

--- app/src/main/java/com/locostatmanager/busines/controllers/DownloadDataController.java
+++ app/src/main/java/com/locostatmanager/busines/controllers/DownloadDataController.java
@@ -1,15 +1,20 @@
 package com.locostatmanager.busines.controllers;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
  * @author dmitry
  */
-@Controller(value = "/downloadData.html")
+@Controller()
+@RequestMapping(value = "/downloadData.htm")
 public class DownloadDataController {
-    
+
+    @RequestMapping(method = RequestMethod.GET)
     public String getPage(Model model) {
         return "downloadData";
     }
