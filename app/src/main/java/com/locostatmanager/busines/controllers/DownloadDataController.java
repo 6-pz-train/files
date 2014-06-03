e53669db0432c19c9a24f0ca997ec1b4ef0a8adc
Anatoliy Shkabara <anatoliy.shkabara@gmail.com>
2014-06-03 23:57:51 +0300

    booom

--- app/src/main/java/com/locostatmanager/busines/controllers/DownloadDataController.java
+++ app/src/main/java/com/locostatmanager/busines/controllers/DownloadDataController.java
@@ -6,22 +6,23 @@ import com.locostatmanager.busines.dao.entities.LocoEntity;
 import com.locostatmanager.busines.exceptions.DataAccessException;
 import com.locostatmanager.busines.exceptions.ValidationException;
 import com.locostatmanager.busines.service.DataLoadingService;
+import com.locostatmanager.busines.service.FileCheckInService;
 import com.locostatmanager.busines.service.LocomotiveService;
-
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -37,6 +38,8 @@ public class DownloadDataController {
     private FileStructureInfoDao fileStructureInfoDao;
     @Autowired
     private DataLoadingService dataLoadingService;
+    @Autowired
+    private FileCheckInService checkInService;
 
     @RequestMapping(method = RequestMethod.GET)
     public String getPage(Model model) {
@@ -48,21 +51,21 @@ public class DownloadDataController {
             MultipartHttpServletRequest request,
             @RequestParam String fileType,
             @RequestParam String locoId,
-            Model model) throws IOException, Exception {
+            Model model) throws Exception {
 
         Map<String, MultipartFile> fileMap = request.getFileMap();
 
-        MultipartFile next = fileMap.values().iterator().next();
-        dataLoadingService.loadData(next.getBytes(), fileType);
+        for (MultipartFile file : fileMap.values()) {
 
-        for(MultipartFile file: fileMap.values()){
-            file.getOriginalFilename();
+            if (!file.getOriginalFilename().contains(".dat")) {
+                throw new ValidationException("Файл має невірне розширення або покодженно!");
+            }
+            checkInService.checkIn(file.getOriginalFilename(), (int) file.getSize());
+            dataLoadingService.loadData(file.getBytes(), fileType);
         }
-        model.addAttribute("success", "Кількість завантаженіх записів, можливо");
+        model.addAttribute("success", "We will rock you!");// сообщение пока не используется, нужно для красивой зеленой строчки
         return "downloadData";
     }
-
-
 
     @ModelAttribute("locomotives")
     public List<LocoEntity> getLocomotives() throws ValidationException, DataAccessException {
@@ -72,5 +75,14 @@ public class DownloadDataController {
     @ModelAttribute("fileTypes")
     public List<FileStructureInfo> getFileNames() throws DataAccessException {
         return fileStructureInfoDao.getAll();
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ModelAndView onException(Exception e, Model model) {
+
+        ModelAndView mv = new ModelAndView("downloadData");
+        mv.addObject("errMsg", e.getMessage());
+        return mv;
     }
 }
