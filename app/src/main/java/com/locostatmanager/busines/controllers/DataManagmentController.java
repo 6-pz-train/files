67b4ff1575d50d6672e2a1a74229402f438daf54
Dmitry Budnik <dmitry_budnik@i.ua>
2014-06-19 03:25:38 +0200

    rest controller

--- app/src/main/java/com/locostatmanager/busines/controllers/DataManagmentController.java
+++ app/src/main/java/com/locostatmanager/busines/controllers/DataManagmentController.java
@@ -1,6 +1,8 @@
 package com.locostatmanager.busines.controllers;
 
+import com.locostatmanager.busines.dao.DataDao;
 import com.locostatmanager.busines.dao.entities.LocoDataEntity;
+import com.locostatmanager.busines.dao.entities.LocoEntity;
 import com.locostatmanager.busines.dao.entities.SensorEntity;
 import com.locostatmanager.busines.exceptions.DataAccessException;
 import com.locostatmanager.busines.exceptions.ValidationException;
@@ -14,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,6 +36,8 @@ public class DataManagmentController {
     private LocomotiveService locomotiveService;
     @Autowired
     private SensorService sensorService;
+    @Autowired
+    private DataDao dataDao;
 
     @RequestMapping(value = "/sensor", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
@@ -60,6 +65,51 @@ public class DataManagmentController {
     public ResponseOK deleteLocomotive(@RequestParam String id) throws ValidationException, DataAccessException {
         locomotiveService.delete(id);
         return new ResponseOK();
+    }
+
+    @RequestMapping(value = "locomotives/add/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseOK addLoco(@PathVariable String id, @RequestParam String title) throws ValidationException, DataAccessException {
+        locomotiveService.add(id, title);
+        return new ResponseOK();
+    }
+
+    @RequestMapping(value = "/locomotives/delete/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseOK deleteLoco(@PathVariable String id) throws ValidationException, DataAccessException {
+        locomotiveService.delete(id);
+        return new ResponseOK();
+    }
+
+    @RequestMapping(value = "/locomotives", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<LocoEntity> locomotives() throws ValidationException, DataAccessException {
+        return locomotiveService.getAll();
+    }
+
+    @RequestMapping(value = "/sensors", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<SensorEntity> getAllSensor() throws DataAccessException, ValidationException {
+
+        return sensorService.getAll();
+    }
+
+    @RequestMapping(value = "/sensors/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public SensorEntity getSensorByName(@PathVariable String name) throws DataAccessException, ValidationException {
+        SensorEntity entity = sensorService.getByName(name);
+        return entity;
+    }
+
+    @RequestMapping(value = "/locomotive/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<LocoDataEntity> getStatistic(@RequestParam String sdate, @RequestParam String edate, @PathVariable String id) throws DataAccessException, ValidationException {
+
+        LocoEntity entity = new LocoEntity();
+        entity.setIdLoco("VL100");
+        entity.setTitleLoco("");
+        List<LocoDataEntity> list = dataDao.getByLocomotive(entity);
+        return list;
     }
 
     @ExceptionHandler({ValidationException.class, DataAccessException.class, Exception.class})
